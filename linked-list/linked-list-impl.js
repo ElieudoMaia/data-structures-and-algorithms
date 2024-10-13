c45c8e4a37b8bfe44a2651d3d1eadd613b310e59
Elieudo Maia <elieudo.maia@gmail.com>
2024-10-13 10:53:04 -0300

    feat: add linked list basic methods

---
+++ linked-list/linked-list-impl.js
@@ -0,0 +1,64 @@
+class Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = new Node(value, null);
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newTail = new Node(value, null);
+    this.tail.next = newTail;
+    this.tail = newTail;
+    this.length += 1;
+    return this;
+  }
+
+  prepend(value) {
+    const newHead = new Node(value, this.head);
+    this.head = newHead;
+    this.length += 1;
+    return this;
+  }
+
+  insert(index, value) {
+    const isIndexOutOfBounds = index < 0 || index >= this.length;
+    if (isIndexOutOfBounds) throw new Error('Index out of bounds');
+
+    if (index === 0) return this.prepend(value);
+    if (index === this.length - 1) return this.append(value);
+
+    let currentNode = this.head;
+    let i = 0;
+
+    while (i !== index - 1) {
+      currentNode = currentNode.next;
+      i += 1;
+    }
+
+    const newNode = new Node(value, currentNode.next);
+    currentNode.next = newNode;
+
+    return this;
+  }
+}
+
+const list = new LinkedList(10);
+console.log(list);
+
+list.append(5).append(16);
+console.log(list);
+
+console.log(list.head.next.next.value);
+
+list.prepend(3);
+console.log(JSON.stringify(list));
+
+list.insert(2, 11);
+console.log(JSON.stringify(list));
